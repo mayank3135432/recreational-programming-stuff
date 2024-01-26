d76a4329517e9035184aea066240c5dc39a5932b
ateium munol <ateium.init@protonmail.com>
2024-01-26 12:01:29 +0530

    snake and ladder backend i guess

---
+++ c_programs/snake_ladder/random_number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+  int i,x,num;
+  srand(time(NULL));
+  for(i=0;i<10;i++){
+    x = rand()%6 +1;
+    printf("%d\n",x);
+  }
+  return 0;
+}
